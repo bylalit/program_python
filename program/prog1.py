cd1c2bad53a8b377d98b2e11e5a5cd6bedb949a3
Lalit0101 <lalitkp0101@gmail.com>
2024-08-07 20:31:06 +0530

    done

---
+++ program/prog1.py
@@ -0,0 +1,29 @@
+# Play Game Program 
+
+cardBal = 10000
+play = 1
+
+while play == 1:
+    print("Enter the play game zone")
+    choice = input("1. Car racing : 200 \n 2. cycling racing : 350 \n 3. jumping boll : 420")
+
+    if choice == 1:
+        if cardBal >= 200:
+            print("Welcome the car racing")
+            cardBal -= 200
+        else:
+            print("Ensaficiant balance")
+    elif choice == 2:
+        if cardBal >= 350:
+            print("Welcome the cycling racing")
+            cardBal -= 350
+        else:
+            print("Ensaficiant balance")
+    elif choice == 3:
+        if cardBal >= 420:
+            print("Welcome the jumping boll")
+            cardBal -= 350
+        else:
+            print("Ensaficiant balance")
+    else:
+        print("Please valid choice number and correct number enter")
