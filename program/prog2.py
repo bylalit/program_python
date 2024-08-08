f4b51c0a73bfbc316350bca24799a3c00049b820
Lalit0101 <lalitkp0101@gmail.com>
2024-08-08 18:16:20 +0530

    okk

--- program/prog2.py
+++ program/prog2.py
@@ -6,3 +6,19 @@ while i <= wantValue:
     value.append(name)
     i+=1
 print(value)
+
+print("You want to add data to presss 1 \nYou want to delete data to presss 2 \nYou want to delete data from the perticular Index to presss 3")
+
+choise = int(input("Enter the valid number : "))
+
+if choise == 1:
+    store = input("Enter the add data item name: ")
+    def add(x):
+        value.append(x)
+        print(value)
+    add(store)
+elif choise == 2:
+    def remove():
+        value.pop()
+        print(value)
+    remove()
