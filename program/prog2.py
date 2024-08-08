f6305bd9c09b674df62db6958fc1562253cefba9
Lalit0101 <lalitkp0101@gmail.com>
2024-08-08 18:31:17 +0530

    okk gok okk

--- program/prog2.py
+++ program/prog2.py
@@ -1,11 +1,11 @@
-# wantValue = int(input("Enter the value: "))
-# i = 1
-# value = []
-# while i <= wantValue:
-#     name = input("Enter the menu name: ")
-#     value.append(name)
-#     i+=1
-# print(value)
+wantValue = int(input("Enter the value: "))
+i = 1
+value = []
+while i <= wantValue:
+    name = input("Enter the menu name: ")
+    value.append(name)
+    i+=1
+print(value)
 
 
 paly = 1 
@@ -20,17 +20,17 @@ while paly == 1:
             value.append(x)
             print(value)
         add(store)
-    elif choise == 2:
-        def remove():
-            value.pop()
-            print(value)
-        remove()
-    elif choise == 3:
-        store = int(input("Enter the index of you want to delete: "))
-        def remove(x):
-            value.pop(x)
-            print(value)
-        remove(store)
+    # elif choise == 2:
+    #     def remove():
+    #         value.pop()
+    #         print(value)
+    #     remove()
+    # elif choise == 3:
+    #     store = int(input("Enter the index of you want to delete: "))
+    #     def remove(x):
+    #         value.pop(x)
+    #         print(value)
+    #     remove(store)
     else:
         print("Please enter the valid choice")
     paly = int(input("If you want to add, delete process again than press 1 othrwise 0 press: "))
