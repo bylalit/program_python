cd955c99f1bf74b0b0e0c8dc8fb708120de40703
Lalit0101 <lalitkp0101@gmail.com>
2024-08-08 20:43:44 +0530

    change data

--- program/prog2.py
+++ program/prog2.py
@@ -22,17 +22,17 @@ while paly == 1:
             value.append(x)
             print(value)
         add(store)
-    # elif choise == 2:
-    #     def remove():
-    #         value.pop()
-    #         print(value)
-    #     remove()
-    # elif choise == 3:
-    #     store = int(input("Enter the index of you want to delete: "))
-    #     def remove(x):
-    #         value.pop(x)
-    #         print(value)
-    #     remove(store)
-    # else:
-    #     print("Please enter the valid choice")
-    # paly = int(input("If you want to add, delete process again than press 1 othrwise 0 press: "))
+    elif choise == 2:
+        def remove():
+            value.pop()
+            print(value)
+        remove()
+    elif choise == 3:
+        store = int(input("Enter the index of you want to delete: "))
+        def remove(x):
+            value.pop(x)
+            print(value)
+        remove(store)
+    else:
+        print("Please enter the valid choice")
+    paly = int(input("If you want to add, delete process again than press 1 othrwise 0 press: "))
