eff0dbff3345b4cf53bee0c66b2687a836927a0e
Lalit0101 <lalitkp0101@gmail.com>
2024-08-08 18:30:48 +0530

    okk done

--- program/prog2.py
+++ program/prog2.py
@@ -1,11 +1,11 @@
-wantValue = int(input("Enter the value: "))
-i = 1
-value = []
-while i <= wantValue:
-    name = input("Enter the menu name: ")
-    value.append(name)
-    i+=1
-print(value)
+# wantValue = int(input("Enter the value: "))
+# i = 1
+# value = []
+# while i <= wantValue:
+#     name = input("Enter the menu name: ")
+#     value.append(name)
+#     i+=1
+# print(value)
 
 
 paly = 1 
