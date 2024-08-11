19767a9cf09b174cd4b8d2b667b4416e4126c5a6
Lalit0101 <lalitkp0101@gmail.com>
2024-08-11 10:16:45 +0530

    donr the program

--- program/prog3.py
+++ program/prog3.py
@@ -24,8 +24,75 @@
 # print("=",count)
 
 
-user = int(input("Enter the start value: "))
-end = int(input("Enter the end value: "))
-for i in range(user, end):
-    if i % 2 == 0:
-        print(i, end=" ")
+# user = int(input("Enter the start value: "))
+# end = int(input("Enter the end value: "))
+# for i in range(user, end):
+#     if i % 2 == 0:
+#         print(i, end=" ")
+
+
+
+# no = 43445
+# r = 0
+
+# while no > 0:
+#     rev = no % 10
+#     r += rev
+#     no //= 10
+    
+# print(r)
+
+
+# no = 1234567
+# r = 0
+
+# while no > 0:
+#     rev = no % 10
+#     r = r * 10 + rev
+#     no //= 10
+# print(r)
+
+
+
+# no = 1234567
+# r = 0
+# for i in range (1, no):
+#     rev = no % 10
+#     r = r * 10 + rev
+#     no //= 10
+
+# print(r)
+
+
+# n1 = 0
+# n2 = 1
+
+# a =int(input("Enter the num: "))
+# print(n1, n2, end=" ")
+# i = 2
+
+# while i <= a:
+#     temp = n1 + n2
+#     print(temp, end=" ")
+    
+#     n1 = n2
+#     n2 = temp
+    
+#     i += 1
+
+
+
+# no = int(input("Enter the number: "))
+# prime = 1
+
+# for i in range(2,no):
+#     if no % i == 0:
+#         prime = 0
+#         break
+    
+# if prime == 1:
+#     print("This is a prime number.")
+# else:
+#     print("This is not a pime number.")
+
+
