9e5195220bb68753774193fb41978016a9d19e79
Lalit0101 <lalitkp0101@gmail.com>
2024-08-12 18:35:47 +0530

    okk done

---
+++ program/prog4.py
@@ -0,0 +1,44 @@
+# fac = 1
+# nam = 5
+# for i in range(1, nam+1):
+#     fac *= i  
+# print(fac)
+
+
+# fac = 1
+# le = 1
+# while 5 >= fac:
+#     le *= fac
+#     fac += 1
+    
+# print(le)
+
+
+
+# num = 34252
+# r = 0
+
+# while num > 0:
+#     rev = num % 10
+#     r += rev
+#     num //= 10
+    
+# print(r)
+
+
+
+# n1 = 0
+# n2 = 1
+# a = 8
+# print(n1, n2, end=" ")
+# i = 2
+# while i < a:
+#     temp = n1 + n2
+#     print(temp, end=" ")
+#     n1 = n2
+#     n2 = temp
+#     i += 1
+
+
+n1 = 0
+n2 = 1
