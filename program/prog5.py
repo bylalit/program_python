21cb10d47c0c7b4cc12ebc512813997e3169931c
Lalit0101 <lalitkp0101@gmail.com>
2024-08-14 19:06:23 +0530

    done the program

--- program/prog5.py
+++ program/prog5.py
@@ -5,3 +5,17 @@ rent = int(input("Enter Your hostel/flate rent = "))
 foodAmount = int(input("Enter the amount of food orderes = "))
 electricity = int(input("Enter the totl of electricity spend = "))
 unit = int(input("Enter the charge par unit = "))
+person = int(input("Enter the number of persons living in room/flat = "))
+
+def a():
+    rentFoodBill = rent + foodAmount
+    electricityBill = electricity * unit
+    
+    fullTotal = rentFoodBill + electricityBill
+    
+    final = int(fullTotal / person)
+    return final
+    
+    
+total = a()
+print("Each person will pay = ",total)
