3cb73dc38104178c22a8c2d7d69c11dca93fc509
Lalit0101 <lalitkp0101@gmail.com>
2024-08-14 18:54:45 +0530

    done the start project

---
+++ program/prog5.py
@@ -0,0 +1,7 @@
+# Rent Calculator Mini Project
+
+
+rent = int(input("Enter Your hostel/flate rent = "))
+foodAmount = int(input("Enter the amount of food orderes = "))
+electricity = int(input("Enter the totl of electricity spend = "))
+unit = int(input("Enter the charge par unit = "))
