80915b8795b565328c87225186149448502e6d13
Lalit0101 <lalitkp0101@gmail.com>
2024-08-17 10:57:22 +0530

    new pro you tube

---
+++ program/prog7.py
@@ -0,0 +1,28 @@
+# name = "Lalit"
+# age = 20
+
+# print("My name is", name, "and i am", age, "years old", sep="|")
+
+# print("Hello world", end=" | ")
+# print("I am", name)
+
+# help(print)
+
+
+# def test_fun(a,b):
+#     """
+#     a: value 1
+#     b: value 2
+    
+#     returns: int
+#     """
+    
+#     return a + b
+
+# help(test_fun)
+
+# print(test_fun(12,5))
+
+rng = range(2,10)
+
+print(list(rng))
