e49a7e2df4ec3f2b005147ba893448d144f0ad33
Lalit0101 <lalitkp0101@gmail.com>
2024-08-21 18:12:55 +0530

    done the

--- program/prog6copy.py
+++ program/prog6copy.py
@@ -1,8 +1,8 @@
-question = ("How many elements are in the periodic table?: ",
-            "Which animal lays the largest eggs?: ",
-            "What is the most gas in Earth's atmosphere?: ",
-            "How many bones are in the human body?: ",
-            "Which planet in the solar system is the hottest?: ")
+questions = ("1. How many elements are in the periodic table?: ",
+            "2. Which animal lays the largest eggs?: ",
+            "3. What is the most gas in Earth's atmosphere?: ",
+            "4. How many bones are in the human body?: ",
+            "5. Which planet in the solar system is the hottest?: ")
 
 ansewr = (("A. 116", "B. 117", "C. 118", "D. 119"),
           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"),
@@ -11,14 +11,29 @@ ansewr = (("A. 116", "B. 117", "C. 118", "D. 119"),
           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars")
           )
 
-ans = ("C", "D", "A", "A", "B")
+anss = ("C", "D", "A", "A", "B")
 gusses = []
-
+score = 0
 index_no = 0
-for i in range(len(question)):
-    print(question[i])
-    for j in range(len(ansewr[index_no])):
-        print(ansewr[index_no][j])
-        index_no +=  1
 
-        # \end{code}
+
+for question in questions:
+    print("------------------------")
+    print(question)
+    for ans in ansewr[index_no]:
+        print(ans)
+    
+    guss = input("Selecte one option: ").upper()
+    gusses.append(guss)   
+        
+    if guss == anss[index_no]:
+        score += 1  
+        print("Correct!")
+    else:
+        print("Wrong!")
+  
+    index_no += 1
+
+print(anss)
+print(gusses)
+print(score)
