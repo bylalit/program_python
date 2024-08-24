14ec2eb35a2008980f17c70eeda446dd426dee20
Lalit0101 <lalitkp0101@gmail.com>
2024-08-24 20:03:45 +0530

    okk done the pro

---
+++ program/prog10.py
@@ -0,0 +1,93 @@
+# 1  program sum of two numbers using function
+# def sum_of_two_numbers(a, b):
+#     return a + b
+# a = 40
+# b = 50
+# print("Sum of two numbers is ", sum_of_two_numbers(a, b))
+
+
+
+
+
+# # 2 program sum of three numbers using function
+# def sum_of_three_numbers(a, b, c):
+#     return a + b + c
+# a = 20
+# b = 30
+# c = 10
+# print("Sum of three numbers is ", sum_of_three_numbers(a, b, c))
+
+
+
+
+
+# 3 program any character
+# def any_character():
+#     char = input("Enter any character: ")
+#     print("The character you entered is: ", char)
+# any_character()
+
+
+
+
+
+# 4 program full name 
+# def full_name():
+#     name = input("Enter your fulll name: ")
+#     print("Your Full Name is: ", name)
+# full_name()
+
+
+
+
+
+# 5 program swaping without third  variable
+# a = 10
+# b = 20
+# a = a + b
+# b = a - b
+# a = a - b
+# print(a)
+# print(b)
+
+
+
+
+
+# 6 program swapping using third variable
+# a = 10
+# b = 20
+# temp = a
+# a = b
+# b = temp
+# print(a)
+# print(b)
+
+
+
+
+
+# 7 program average of three number
+# a = 70
+# b = 80
+# c = 50
+# avg =  (a + b + c) / 3
+# print("The average of three number is: ", int(avg))
+
+
+
+
+
+# 8 program celcius to ferenhiet
+# c = 30
+# f = (c * 9/5) + 32
+# print("The temperature in ferenhiet is: ", f)
+
+
+
+
+
+#  9 program ferenhiet to celcius
+# f = 86
+# c = (f - 32) * 5/9
+# print("The temperature in celcius is: ", c)
