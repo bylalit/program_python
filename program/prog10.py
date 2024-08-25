f7625c4551a377e64fae6d99467d27154f623f9f
Lalit0101 <lalitkp0101@gmail.com>
2024-08-25 18:25:31 +0530

    new file add

--- program/prog10.py
+++ program/prog10.py
@@ -228,16 +228,16 @@
 
 
 # 18 program maximum out of four number
-a = 303
-b = 262
-c = 50
-d = 2055
+# a = 303
+# b = 262
+# c = 50
+# d = 2055
 
-if a > b and a > c and a > d:
-    print("A is maximum")
-elif b > a and b > c and b > d:
-    print("B is maximum")
-elif c > a and c > b and c > d:
-    print("C is maximum")
-else:
-    print("D is maximum")
+# if a > b and a > c and a > d:
+#     print("A is maximum")
+# elif b > a and b > c and b > d:
+#     print("B is maximum")
+# elif c > a and c > b and c > d:
+#     print("C is maximum")
+# else:
+#     print("D is maximum")
