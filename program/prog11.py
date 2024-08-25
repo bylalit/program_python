ec421e13db8411f3bbea9778d196e57aff772c2c
Lalit0101 <lalitkp0101@gmail.com>
2024-08-25 18:36:51 +0530

    okk done the

---
+++ program/prog11.py
@@ -0,0 +1,25 @@
+item = []
+price = []
+total = 0
+
+again = 0
+
+while again != 1:
+    food = input("Enter a food to buy (q to quit): ")
+    
+    if  food.lower() == 'q':
+        again = 1
+        break
+    
+    item.append(food)
+    amount = float(input(f"Enter the amount of a {food}: $"))
+    price.append(amount)
+    total += amount
+
+print("\n")
+print("\n")
+print("-----YOUR CARt-----")
+for i in item:
+    print(i, end=" ")
+    
+print(f"Your total is: ${total}")
