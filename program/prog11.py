2a7806711156394089a9a7116230c60a4d067702
Lalit0101 <lalitkp0101@gmail.com>
2024-08-25 19:59:54 +0530

    new commit data

--- program/prog11.py
+++ program/prog11.py
@@ -1,26 +1,26 @@
-item = []
-price = []
-total = 0
+# item = []
+# price = []
+# total = 0
 
-again = 0
+# again = 0
 
-while again != 1:
-    food = input("Enter a food to buy (q to quit): ")
+# while again != 1:
+#     food = input("Enter a food to buy (q to quit): ")
     
-    if  food.lower() == 'q':
-        again = 1
-        break
+#     if  food.lower() == 'q':
+#         again = 1
+#         break
     
-    item.append(food)
-    amount = float(input(f"Enter the amount of a {food}: $"))
-    price.append(amount)
-    total += amount
+#     item.append(food)
+#     amount = float(input(f"Enter the amount of a {food}: $"))
+#     price.append(amount)
+#     total += amount
 
 
-print("\n")
-print("-----YOUR CART-----")
-for i in item:
-    print(i, end=" ")
-print("")    
-print(f"Your total is: ${total}")
-print("\n")
+# print("\n")
+# print("-----YOUR CART-----")
+# for i in item:
+#     print(i, end=" ")
+# print("")    
+# print(f"Your total is: ${total}")
+# print("\n")
