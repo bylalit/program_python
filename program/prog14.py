1c3cbe718cd4ef20632488f7a67b8044b6bd8e97
Lalit0101 <lalitkp0101@gmail.com>
2024-08-29 21:43:05 +0530

    okk

--- program/prog14.py
+++ program/prog14.py
@@ -138,10 +138,10 @@ import math
 #     elif option == 4:
 #         principal = float(input("Enter the principal amount: "))
 #         rate = float(input("Enter the rate of interest: "))
-#         time = float(input("Enter the time in years: "))
+        time = float(input("Enter the time in years: "))
         
-#         simple_interest = (principal * rate * time) / 100
-#         print(f"The simple interest is: {simple_interest}")
+        simple_interest = (principal * rate * time) / 100
+        print(f"The simple interest is: {simple_interest}")
         
 #     elif option == 5:
 #         choice = 1
