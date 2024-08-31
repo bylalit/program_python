fa3986e5e99034deb17d365352572ab3ce204b8a
Lalit0101 <lalitkp0101@gmail.com>
2024-08-31 20:38:14 +0530

    thank okk

--- program/prog16.py
+++ program/prog16.py
@@ -66,4 +66,13 @@
 
 
 with open("practice.txt", "r") as f:
+    data = f.read()
+    # print(data)
+    
+new_data = data.replace("Java", "Python")
+print(new_data)
+
+
+with open("practice.txt", "w") as f:
+    f.write(new_data)
     
