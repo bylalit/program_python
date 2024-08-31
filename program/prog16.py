cbaa7ffd6b4186411fd62e2d745171abacff3372
Lalit0101 <lalitkp0101@gmail.com>
2024-08-31 19:50:33 +0530

    okk done

---
+++ program/prog16.py
@@ -0,0 +1,20 @@
+f = open("D:\program_py\program_python\program\demo.txt", "w")
+
+
+#   reading a file mode! 
+
+# data = f.read()
+# data = f.readline()
+# print(data)
+# print(type(data))
+
+# f.close()
+
+
+
+
+# writting  a file mode! 
+
+f.write("I want to learn javascrpit 123.")
+
+f.close()
