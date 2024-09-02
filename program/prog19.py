d7fe1c645eb5ca23b03d7c7cd53e3f9a36e2eb72
Lalit0101 <lalitkp0101@gmail.com>
2024-09-02 17:58:57 +0530

    okk

---
+++ program/prog19.py
@@ -0,0 +1,38 @@
+# random function
+
+import random
+ 
+# n = random.randint(2,8)
+# print(n)
+
+# n1 = random.randrange(2,4)
+# print(n1)
+
+
+# l = [10,20,30,40]
+# n2 = random.choice(l)
+# print(n2)
+
+
+# r = random.random()*5   #only 0 and 1 give the value.
+# print(int(r))
+
+
+# l = [10,20,30,40]
+# random.shuffle(l)
+# print(l)
+
+
+# u = random.uniform(3,9)     #floating  point number give  the value.
+# print(u)
+
+
+
+
+
+
+
+# program 1.
+# gusees the  random number between 1 to 100
+
+
