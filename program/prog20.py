fb505589c360c8399bcef364e3b9a21f28e07482
Lalit0101 <lalitkp0101@gmail.com>
2024-09-05 19:23:06 +0530

    okk doe the

--- program/prog20.py
+++ program/prog20.py
@@ -4,7 +4,7 @@ print("Welcome to ABC Bank\n\n")
 print("Insert your ATM card")
 
 password = 1234
-
+balance = 10000
 pin = int(input("Enter your Four digit pin: "))
 
 
@@ -12,9 +12,14 @@ choice = 0
 if pin == password:
     
     while choice != 4:
-        print("****Menu****\n1. ** Balance\n2. ** Deposit\n3. ** Withdraw\n4. ** Cancel")
+        choice = int(input("****Menu****\n1. ** Balance\n2. ** Deposit\n3. ** Withdraw\n4. ** Cancel"))
     
-    
+        if choice == 1:
+            print("Your corrent balance is: Rs.", balance)
+            
+        elif choice == 2:
+            
+
     
 else:
     print("Incorrect password try again!")
