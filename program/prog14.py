66a5bb4384c15c8d2011b9ebe6851e671bc93bbf
Lalit0101 <lalitkp0101@gmail.com>
2024-09-06 20:16:37 +0530

    done okk 6

--- program/prog14.py
+++ program/prog14.py
@@ -485,17 +485,17 @@ import math
 
 # 6. progaram
 
-# num = int(input("Enter the number: "))
-
-# if num > 1:
-#     for i in  range(2, num):
-#         if num %  i == 0:
-#             print(num,  "is not a prime number")
-#             break
+# def is_prime(num):
+#     if num > 1:
+#         for i in  range(2, num):
+#             if num %  i == 0:
+#                 print(num,  "is not a prime number")
+#                 break
+#         else:
+#             print(num, "is a prime number")
 #     else:
-#         print(num, "is a prime number")
-# else:
-#     print(num, "is not a prime number")
+#         print(num, "is not a prime number")
 
-    
+# num = int(input("Enter the number: "))
+# is_prime(num)
 
