644baca205d3aec56c8505b158e7b6e97ea97b92
Lalit0101 <lalitkp0101@gmail.com>
2024-09-12 09:34:00 +0530

    okk

--- program/prog18.py
+++ program/prog18.py
@@ -1,105 +1,169 @@
 # this program search for the data in the given object of dict..
 
 student = {
-    "lalit": {
-        "name": "lalit",
-        "Age": 25,
-        "Grade": "A",
-        "blood": "AB",
-        "gender":"male",
-        "address": "Ahmedabad",
+    "rakesh" : {
+        "fname": 'prajapati',
+        'mname': 'rakesh',
+        'lname': 'rameshbhai',
+        'age': 18,
+        'gender': 'male',
+        'area': 'ranip',
+        'add': 'c,13 rajendra park, ranip',
+        'pincode': 380787,
+        'blood_group': 'B+',
     },
-    "vidhi": {
-        "name": "vidhi",
-        "Age":  22,
-        "Grade": "E",
-        "blood": "AB",
+    "vidhi" : {
+        "fname": 'nagar',
+        'mname': 'vidhi',
+        'lname': 'bharatbhai',
+        'age': 20,
+        'gender': 'female',
+        'area': 'odhav',
+        'add': 'c,13 arbudhanager, odhav, ahemdhabad',
+        'pincode': 382024,
+        'blood_group': 'B+',
+    },
+    "manshi" : {
+        "fname": 'thakor',
+        'mname': 'manshi',
+        'lname': 'saileshbhai',
+        'age': 17,
+        'gender': 'female',
+        'area': 'rajendra',
+        'add': '65 mukeshnagar odhav',
+        'pincode': 382024,
+        'blood_group': 'A+',
+    },
+    "mittal" : {
+        "fname": 'prajapati',
+        'mname': 'mittal',
+        'lname': 'sureshbhai',
+        'age': 22,
+        'gender': 'female',
+        'area': 'cmc',
+        'add': '25 ctm char rasta odhav',
+        'pincode': 382415,
+        'blood_group': 'AB',
+    },
+    "lalit" : {
+        "fname": 'prajapati',
+        'mname': 'lalit',
+        'lname': 'dilipbhai',
+        'age': 18,
+        'gender': 'male',
+        'area': 'odhav',
+        'add': 'c, 25 rajendra park, ranip',
+        'pincode': 382415,
+        'blood_group': 'AB',
+    },
+    "sid" : {
+        "fname": 'deshai',
+        'mname': 'sid',
+        'lname': 'rakeshbhai',
+        'age': 25,
+        'gender': 'male',
+        'area': 'odhav',
+        'add': '78 rajendra park, ranip, ahemdhabad',
+        'pincode': 382024,
+        'blood_group': 'B+',
+    },
+    "aadity" : {
+        "fname": 'puvar',
+        'mname': 'aadity',
+        'lname': 'rakeshbhai',
+        'age': 28,
+        'gender': 'male',
+        'area': 'ranip',
+        'add': '56 rajendra park, ranip, ahemdhabad',
+        'pincode': 382024,
+        'blood_group': 'B+',
+    },
+    "rishi" : {
+        "fname": 'prajapati',
+        'mname': 'rishi',
+        'lname': 'rajubhai',
+        'age': 23,
+        'gender': 'male',
+        'area': 'odhav',
+        'add': '56 tulshi park',
+        'pincode': 382024,
+        'blood_group': 'B+',
+    },
+    "sneh" : {
+        "fname": 'bharvad',
+        'mname': 'sneh',
+        'lname': 'sureshbhai',
+        'age': 30,
+        'gender': 'male',
+        'area': 'shitalchaya',
+        'add': '16 vrundhavan vatica',
+        'pincode': 382415,
+        'blood_group': 'AB',
+    },
+    "jay" : {
+        "fname": 'deshai',
+        'mname': 'jay',
+        'lname': 'deneshbhai',
+        'age': 16,
+        'gender': 'male',
+        'area': 'bhavani nagar',
+        'add': '34 hastinapur socity',
+        'pincode': 382415,
+        'blood_group': 'AB-',
+    },
+        "riddhi":{
+        "fname":"nagar",
+        "mname":"ridhi",
+        "lname":"bharatbhai",
         "gender":"female",
-        "address": "pune"
+        "area":"vastral",
+        "address":"404 vrundavan vatika",
+        "pincode":382415,
+        "age":20,
+        "blood_group":"A+",
     },
-    "suresh": {
-        "name": "suresh",
-        "Age": 30,
-        "Grade": "B",
-        "blood": "O",
+    "bhavya":{
+        "fname":"nagar",
+        "mname":"bhavya",
+        "lname":"nileshbhai",
         "gender":"male",
-        "address": "surat"
+        "area":"rajasthan",
+        "address":"34 jogeshwari",
+        "pincode":382415,
+        "age":20,
+        "blood_group":"A+",
     },
-    "ramesh": {
-        "name":  "ramesh",
-        "Age": 35,
-        "Grade":  "C",
-        "blood": "A",
+    "ronak":{
+        "fname":"parmar",
+        "mname":"ronak",
+        "lname":"rajubhai",
         "gender":"male",
-        "address": "rajkot"
+        "area":"ranip",
+        "address":"20  jogeshwari",
+        "pincode":382415,
+        "age":20,
+        "blood_group":"A+",
     },
-    "roshni":{
-        "name": "roshni",
-        "Age": 20,
-        "Grade": "D",
-        "blood":  "B",
-        "gender":"female",
-        "address": "mumbai"
+    "suresh":{
+        "fname":"prajapati",
+        "mname":"suresh",
+        "lname":"maheshbhai",
+        "gender":"male",
+        "area":"odhav",
+        "address":"20 maheshvari",
+        "pincode":382024,
+        "age":23,
+        "blood_group":"AB",
     },
-    "vidhi": {
-        "name": "vidhi",
-        "Age":  22,
-        "Grade": "E",
-        "blood": "AB",
-        "gender":"female",
-        "address": "pune"
+    "ronak":{
+        "fname":"parmar",
+        "mname":"ronak",
+        "lname":"rajubhai",
+        "gender":"male",
+        "area":"ranip",
+        "address":"20  jogeshwari",
+        "pincode":382415,
+        "age":20,
+        "blood_group":"A+",
     },
 }
-
-for name in student:
-    print()
-    print(name)
-    for key, val in student[name].items():
-        print(key.capitalize(), "  ==>  " , val)
-print()
-
-print("""---------------------------------------
-        what will be search ?
-            1. Name
-            2. Age
-            3. Grade
-            4. Blood
-            5. Gender
-            6. Address
-            7. Exit      
-    """)
-
-start = 1
-
-while start == 1:
-    keyValue = input("Enter the key  to search: ").capitalize()
-
-    if keyValue == "Exit":
-        start = 0
-    else:
-        if keyValue == "Age":
-            valueSearch = int(input("Enter the value to search: "))
-        else:
-            valueSearch = input("Enter the  value to search: ").capitalize()
-
-    print()
-    for i in student:
-        for k, v in student[i].items():
-            if  k.capitalize() == keyValue:
-                if keyValue == "Age":
-                    if v == valueSearch:
-                        print(i.upper())
-                        print(k.capitalize(), "==>" , v, "\n")
-                else:
-                    if v.capitalize() == valueSearch:
-                        print(i.upper())
-                        print(k.capitalize(), "==>" , v.capitalize(),  "\n")
-
-    print()
-    
-    if keyValue == "Exit":
-        start = 0
-    else:
-        start = int(input("Can  you search again ? 1. yes 2. no: "))
-    print(" Thank you for using this program. ")
-    
