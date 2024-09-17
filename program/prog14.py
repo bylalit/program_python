188befc8082bb9239b15314e227042984e8cc73d
Lalit0101 <lalitkp0101@gmail.com>
2024-09-17 20:57:42 +0530

    okk

--- program/prog14.py
+++ program/prog14.py
@@ -528,11 +528,11 @@ import math
 
 # 10. progaram
 
-def variable_length(*a):
-    for i in range(0, len(a)):
-        print(a[i])
+# def variable_length(*a):
+#     for i in range(0, len(a)):
+#         print(a[i])
 
-variable_length('a','b','c','d','e','f','g','h')
+# variable_length('a','b','c','d','e','f','g','h')
 
 
 
