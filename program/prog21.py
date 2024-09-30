a1d16ba890f8244dc0aa32319669b7c41169aa0d
Lalit0101 <lalitkp0101@gmail.com>
2024-09-30 10:42:47 +0530

    done the

--- program/prog21.py
+++ program/prog21.py
@@ -1,13 +1,27 @@
 class Car:
     def __init__(self, brand, model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
         
-    def full_name(self):
-        return f"{self.brand} {self.model}"
+    def get_brand(self):
+        return self.__brand + " ! "
         
-    
-my_car = Car("toyota", "corolla")
-print(my_car.brand)
-print(my_car.model)
-print(my_car.full_name())
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+
+my_tesla = ElectricCar("Tesla", "Shular", "85KWH")
+# print(my_tesla.brand)
+print(my_tesla.get_brand())
+
+# my_car = Car("toyota", "corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
