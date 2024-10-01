802f4d51874e453aad7f4240eb72727b48fa436f
Lalit0101 <lalitkp0101@gmail.com>
2024-10-01 12:00:56 +0530

    okk done

--- program/prog22.py
+++ program/prog22.py
@@ -5,3 +5,11 @@ class student:
         self.marks = marks
         
         
+    def display(self):
+        print("Name: ", self.name)
+        print("Age: ", self.age)
+        print("Marks: ", self.marks)
+        
+s = student("Raj", "20","89")
+
+s.display()
