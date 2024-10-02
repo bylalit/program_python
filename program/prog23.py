04ce80fb77380a119d975ae292f908fe81b58534
Lalit0101 <lalitkp0101@gmail.com>
2024-10-02 19:30:29 +0530

    okk done

--- program/prog23.py
+++ program/prog23.py
@@ -1,4 +1,13 @@
 
 start = int(input("How many var add data: "))
 
+dic = {}
+while start > 0:
+    key = input("Enter the Keys: ").capitalize()
 
+    val = input("Enter the value: ")
+
+    dic[key] = val
+
+    print(dic)
+    start -= 1
