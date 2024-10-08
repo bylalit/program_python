039328908bd00c38f0a993b55c4b165a465e62cd
Lalit0101 <lalitkp0101@gmail.com>
2024-10-08 20:00:42 +0530

    okk done print data

---
+++ program/pro24.py
@@ -0,0 +1,2 @@
+print("------------------To do App------------------")
+print("1. Enter 'list' to show all tasks \n2. Enter 'add' to add task \n3. 'delete' to remove a task \n4. Enter 'quit' to quit todo: ")
