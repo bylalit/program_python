961d20bbb6fcde432115969b88930f36aa308f7d
Lalit0101 <lalitkp0101@gmail.com>
2024-10-08 20:59:19 +0530

    done

--- program/pro24.py
+++ program/pro24.py
@@ -2,7 +2,7 @@ print("------------------To do App------------------")
 print("1. Enter 'list' to show all tasks \n2. Enter 'add' to add task \n3. 'delete' to remove a task \n4. Enter 'quit' to quit todo: ")
 
 
-item = []
+# item = []
 
 while True:
     input_data = input("Please Enter Your request: ")
