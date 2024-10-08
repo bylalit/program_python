a8faa9221a0886aab77acc9a6f9c2e59e131473c
Lalit0101 <lalitkp0101@gmail.com>
2024-10-08 20:59:06 +0530

    okk done the

--- program/pro24.py
+++ program/pro24.py
@@ -1,2 +1,22 @@
 print("------------------To do App------------------")
 print("1. Enter 'list' to show all tasks \n2. Enter 'add' to add task \n3. 'delete' to remove a task \n4. Enter 'quit' to quit todo: ")
+
+
+item = []
+
+while True:
+    input_data = input("Please Enter Your request: ")
+    if input_data == 'quit':
+        print("Quit Task")
+        break
+    
+    if input_data == 'list':
+        print("--------------")
+        for i in item:
+            print(f"{i} {item[i]}")
+        # print(item)
+        print("--------------")
+    elif input_data == 'add':
+        add = input("Enter your task to add: ")
+        item.append(add)
+        print("Task added")
