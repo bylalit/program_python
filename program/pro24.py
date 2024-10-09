6c37876864f446f30ef09f4ee05008edd8449519
Lalit0101 <lalitkp0101@gmail.com>
2024-10-09 19:27:59 +0530

    okk done

--- program/pro24.py
+++ program/pro24.py
@@ -1,24 +1,24 @@
-# print("------------------To do App------------------")
-# print("1. Enter 'list' to show all tasks \n2. Enter 'add' to add task \n3. 'delete' to remove a task \n4. Enter 'quit' to quit todo: ")
+print("------------------To do App------------------")
+print("1. Enter 'list' to show all tasks \n2. Enter 'add' to add task \n3. 'delete' to remove a task \n4. Enter 'quit' to quit todo: ")
 
 
-# item = []
+item = []
 
-# while True:
-#     input_data = input("Please Enter Your request: ")
-#     count = 0
-#     if input_data == 'quit':
-#         print("Quit Task")
-#         break
+while True:
+    input_data = input("Please Enter Your request: ")
+    count = 0
+    if input_data == 'quit':
+        print("Quit Task")
+        break
     
-#     if input_data == 'list':
-#         print("--------------")
-#         for i in item:
-#             count += 1
-#             print(f"{count}. {i}")
-#         print("--------------")
-#     elif input_data == 'add':
-#         add = input("Enter your task to add: ")
-#         item.append(add)
-#         print("Task added")
+    if input_data == 'list':
+        print("--------------")
+        for i in item:
+            count += 1
+            print(f"{count}. {i}")
+        print("--------------")
+    elif input_data == 'add':
+        add = input("Enter your task to add: ")
+        item.append(add)
+        print("Task added")
         
