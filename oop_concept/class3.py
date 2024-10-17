1d193e8d84d962d827d97d17e715442c7a994372
Lalit0101 <lalitkp0101@gmail.com>
2024-10-17 10:15:29 +0530

    okk new file create

--- oop_concept/class3.py
+++ oop_concept/class3.py
@@ -1,7 +1,8 @@
 class Human:
-    def __init__(self):
+    def __init__(self, num_heart):
         self.num_eyes = 2
         self.nose = 1
+        self.num_heart = num_heart
         
     def eat(self):
         print("Human is eating")
@@ -10,8 +11,8 @@ class Human:
         print("Human is working")
         
 class Male(Human):
-    def __init__(self, name):
-        super().__init__()
+    def __init__(self, name, heart):
+        super().__init__(heart)
         self.name = name
         
     def flirt(self):
@@ -20,11 +21,18 @@ class Male(Human):
     def work(self):
         super().work()
         print("I can working coding")
+        
+    def diaplay(self):
+        print(f"Hi, I am {self.name} and I have {self.num_heart} heart.")
 
 print()
-male_1 = Male("Lalit")
+male_1 = Male("Lalit", 2)
 # male_1.flirt()
 # male_1.work()
 # print(male_1.num_eyes)
-print(male_1.name)
+
+
+# print(male_1.name)
+# print(male_1.num_heart)
+male_1.diaplay()
 print()
