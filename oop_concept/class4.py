17960822ad63adc047b80e5c4976c2c9f54263eb
Lalit0101 <lalitkp0101@gmail.com>
2024-10-17 10:28:49 +0530

    okk

--- oop_concept/class4.py
+++ oop_concept/class4.py
@@ -11,12 +11,19 @@ class Male:
     def work(self):
         print("I acn coding")
     
+# class Female():
+#     pass
+
 class Boy(Human, Male):
-    pass
+    def sleep(self):
+        print("I can sleep")
+    def work(self):
+        print("I can Testing")
 
 boy_1 = Boy()
 # boy_1.eat()
 # boy_1.flirt()
-# boy_1.work()
+boy_1.work()
+print(Boy.mro())
 
-Human.work(boy_1)
+# Human.work(boy_1)
