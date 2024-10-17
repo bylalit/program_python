51615347b5c45b739ff040237e2dbd809aff40a2
Lalit0101 <lalitkp0101@gmail.com>
2024-10-17 10:42:55 +0530

    okk

--- oop_concept/class4.py
+++ oop_concept/class4.py
@@ -1,4 +1,8 @@
 class Human:
+    def __init__(self):
+        self.num_eyes = 2
+        self.noise = 1
+    
     def eat(self):
         print("I am eat")
     def work(self):
@@ -6,6 +10,9 @@ class Human:
     
     
 class Male:
+    def __init__(self, name):
+        self.name = name
+    
     def flirt(self):
         print("I can fliert")
     def work(self):
@@ -15,15 +22,22 @@ class Male:
 #     pass
 
 class Boy(Human, Male):
+    def __init__(self, names):
+        Human.__init__(self)
+        Male.__init__(self, names)
+    
     def sleep(self):
         print("I can sleep")
     def work(self):
         print("I can Testing")
 
-boy_1 = Boy()
+boy_1 = Boy("Lalit")
 # boy_1.eat()
 # boy_1.flirt()
-boy_1.work()
-print(Boy.mro())
+# boy_1.work()
+# print(Boy.mro())
 
 # Human.work(boy_1)
+
+
+print(boy_1.name)
