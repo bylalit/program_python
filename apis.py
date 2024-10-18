4a2de7e36a2f3109460c41a5c6b02d30168036ba
Lalit0101 <lalitkp0101@gmail.com>
2024-10-18 19:07:48 +0530

    new api file created

---
+++ apis.py
@@ -0,0 +1,24 @@
+import requests
+
+def feacth_api_user():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
+    
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        country = user_data["location"]["country"]
+        return username, country
+    else:
+        raise Exception("failed to fecth user data")
+    
+def main():
+    try:
+        username, country = feacth_api_user()
+        print(f"Username: {username} \nCountry: {country}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    main()
