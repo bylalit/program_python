7d447dbb356845fe1dea3715dd65cdc634c188c3
Lalit0101 <lalitkp0101@gmail.com>
2024-10-18 19:42:31 +0530

    okk done api folder

--- apis.py
+++ apis.py
@@ -1,6 +1,34 @@
+# import requests
+
+# def feacth_api_user():
+#     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+#     response = requests.get(url)
+#     data = response.json()
+    
+#     if data["success"] and "data" in data:
+#         user_data = data["data"]
+#         username = user_data["login"]["username"]
+#         country = user_data["location"]["country"]
+#         return username, country
+#     else:
+#         raise Exception("failed to fecth user data")
+    
+# def main():
+#     try:
+#         username, country = feacth_api_user()
+#         print(f"Username: {username} \nCountry: {country}")
+#     except Exception as e:
+#         print(f"Error: {str(e)}")
+
+# if __name__ == "__main__":
+#     main()
+    
+    
+
+
 import requests
 
-def feacth_api_user():
+def user_api():
     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
     response = requests.get(url)
     data = response.json()
@@ -15,10 +43,12 @@ def feacth_api_user():
     
 def main():
     try:
-        username, country = feacth_api_user()
+        username, country = user_api()
         print(f"Username: {username} \nCountry: {country}")
     except Exception as e:
         print(f"Error: {str(e)}")
-
+        
 if __name__ == "__main__":
     main()
+    
+    
--- unit_3/prog7.py
+++ unit_3/prog7.py
@@ -1,39 +1,48 @@
-import sys
+# import sys
 
-class Bank(object):
-    def  __init__(self, name, balance=0.0):
-        self.name = name
-        self.balance = balance
+# class Bank(object):
+#     def  __init__(self, name, balance=0.0):
+#         self.name = name
+#         self.balance = balance
         
-    def deposite(self, amount):
-        self.balance += amount
-        return  self.balance
+#     def deposite(self, amount):
+#         self.balance += amount
+#         return  self.balance
 
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Low Balance, Can Not WithDrow")
-        else:
-            self.balance -= amount
-            return self.balance
+#     def withdraw(self, amount):
+#         if amount > self.balance:
+#             print("Low Balance, Can Not WithDrow")
+#         else:
+#             self.balance -= amount
+#             return self.balance
         
-name = input("Enter Name: ")
+# name = input("Enter Name: ")
 
-b = Bank(name)
+# b = Bank(name)
 
-while True:
-    print("d/D -deposite, w/W -withdrawal, e/E -exits")
-    choice = input("Enter Your Choice: ")
-    print()
+# while True:
+#     print("d/D -deposite, w/W -withdrawal, e/E -exits")
+#     choice = input("Enter Your Choice: ")
+#     print()
     
-    if choice == "e" or choice == "E":
-        print("Thanks for visiting our Bank")
-        print()
-        sys.exit()
+#     if choice == "e" or choice == "E":
+#         print("Thanks for visiting our Bank")
+#         print()
+#         sys.exit()
         
-    amount = float(input("Enter amount: "))
-    if  choice == "d" or choice == "D":
-        print("Balance after deposite: ", b.deposite(amount))
-    elif  choice == "w" or choice == "W":
-        print("Balance after withdrawal: ", b.withdraw(amount))
+#     amount = float(input("Enter amount: "))
+#     if  choice == "d" or choice == "D":
+#         print("Balance after deposite: ", b.deposite(amount))
+#     elif  choice == "w" or choice == "W":
+#         print("Balance after withdrawal: ", b.withdraw(amount))
 
 
+
+
+
+
+
+print("hii")
+# print(dir())
+print(__name__)
+print(__name__ == "__main__")
