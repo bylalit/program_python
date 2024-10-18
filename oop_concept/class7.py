02e26967de1a38ac269248914357cc59a07b0ecf
Lalit0101 <lalitkp0101@gmail.com>
2024-10-18 16:55:51 +0530

    okkk new uit started

--- oop_concept/class7.py
+++ oop_concept/class7.py
@@ -1,5 +1,8 @@
 # file handling in python
+# D:\program_py\program_python\oop_concept\class7.py
 
-f1 = open("file_1.txt", "r")
+
+f1 = open("oop_concept\file_4.txt", "w")
 data = f1.read()
 print(data)
+
