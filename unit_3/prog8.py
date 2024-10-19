aaddeac071fc63c1da036a9985cf5cf7509f8710
Lalit0101 <lalitkp0101@gmail.com>
2024-10-19 18:14:19 +0530

    okk done the

---
+++ unit_3/prog8.py
@@ -0,0 +1,20 @@
+class Emp:
+    def __init__(self, id, name, sal):
+        self.id = id
+        self.name = name
+        self.salary = sal
+        
+    def display(self):
+        print("Id = ", self.id)
+        print("Name = ", self.name)
+        print("Salary = ", self.salary)
+        
+class myClass:
+    @staticmethod
+    def myMethod(e):
+        e.salary += 1000
+        e.display()
+        
+f = Emp(1, "Rahul", 5000)
+myClass.myMethod(f)
+
