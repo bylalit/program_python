38f5b4d9c52fb467d42686b10ecb717826077eaf
Lalit0101 <lalitkp0101@gmail.com>
2024-10-20 22:37:41 +0530

    done

--- mini-project/choose_your_own_adventure.py
+++ mini-project/choose_your_own_adventure.py
@@ -1,10 +1,10 @@
-# name = input("Type your name: ")
-# print("Welcome", name, "to this adventure!")
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
 
-# answer = input("You are on a dirt road, it has come to an end you can go left or right. Which way would you like to go? ").lower()
+answer = input("You are on a dirt road, it has come to an end you can go left or right. Which way would you like to go? ").lower()
 
-# if answer == "left":
-#     print()
-# elif answer == "right":
-#     print()
+if answer == "left":
+    print()
+elif answer == "right":
+    print()
     
