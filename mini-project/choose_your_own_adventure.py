aaaf1700b0fdcf45df38a189fd6209e656f40d10
Lalit0101 <lalitkp0101@gmail.com>
2024-10-20 22:39:12 +0530

    okk

--- mini-project/choose_your_own_adventure.py
+++ mini-project/choose_your_own_adventure.py
@@ -3,8 +3,8 @@ print("Welcome", name, "to this adventure!")
 
 answer = input("You are on a dirt road, it has come to an end you can go left or right. Which way would you like to go? ").lower()
 
-if answer == "left":
-    print()
-elif answer == "right":
-    print()
+# if answer == "left":
+#     print()
+# elif answer == "right":
+#     print()
     
