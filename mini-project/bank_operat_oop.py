6fc2c6d1c7fa14b14e90462ea0751716513aa756
Lalit0101 <lalitkp0101@gmail.com>
2024-10-21 19:05:28 +0530

    okk done half

--- mini-project/bank_operat_oop.py
+++ mini-project/bank_operat_oop.py
@@ -9,8 +9,19 @@ class Bank:
         self.username = username
         self.pan = pan
         self.address = address
+        self.balance = 0.0
         print(f'Hello {self.username} cong! your account created successfully!')
-        
+
+
+    def deposite(self, amount):
+        self.balance = self.balance + amount
+        print(f'{amount} deposited successfully!')
+
+
+print()
+print(f'Welcome to {Bank.bankname}, {Bank.branch}')      
+print()
+
 username = input('Enter Your Name: ')
 pan = input("Enter PAN card number: ")
 address = input("Enter Your Address: ")
