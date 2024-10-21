cdf42bf03483a68b4d3d07820e33493001ba65ad
Lalit0101 <lalitkp0101@gmail.com>
2024-10-21 12:17:56 +0530

    done the site

--- unit_3/prog8.py
+++ unit_3/prog8.py
@@ -19,13 +19,24 @@ f = Emp(1, "Rahul", 5000)
 myClass.myMethod(f)
 
 
+
 # class Emp:
 #     def __init__(self, id, name, sal):
 #         self.id = id
 #         self.name = name
 #         self.salary = sal
+        
 #     def display(self):
 #         print("Id = ", self.id)
 #         print("Name = ", self.name)
-#         print("Salary = ",  self.salary)
+#         print("Salary = ", self.salary)
+        
+# class myClass:
+#     @classmethod
+#     def myMethod(cls, e):
+#         e.salary += 1000
+#         e.display()
+        
+# f = Emp(1, "Rahul", 5000)
+# myClass.myMethod(f)
             
