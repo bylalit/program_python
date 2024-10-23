e3d9295955cd8d9b45b8119ab408397c2df66628
Lalit0101 <lalitkp0101@gmail.com>
2024-10-23 19:14:42 +0530

    okk new progarm start

--- unit_2/prog1.py
+++ unit_2/prog1.py
@@ -86,16 +86,16 @@
 
 
 # 15 program
-list1 = [i for i in range(1,6)]
-list2 = [i for i in range(7,11)]
-print(list1)
-print(list2)
-print()
-list3 = list1 + list2
-print(list3)
+# list1 = [i for i in range(1,6)]
+# list2 = [i for i in range(7,11)]
+# print(list1)
+# print(list2)
+# print()
+# list3 = list1 + list2
+# print(list3)
 
-new_list = [i for i in list3 for x in (0,1)]
-print(new_list)
+# new_list = [i for i in list3 for x in (0,1)]
+# print(new_list)
 
-list_cloneing = list3[:]
-print(list_cloneing)
+# list_cloneing = list3[:]
+# print(list_cloneing)
