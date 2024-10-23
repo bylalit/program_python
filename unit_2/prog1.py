651a9793cf1586b7981d2922aed09d9ce341a907
Lalit0101 <lalitkp0101@gmail.com>
2024-10-23 18:35:02 +0530

    copalet one program

---
+++ unit_2/prog1.py
@@ -0,0 +1,14 @@
+
+# 22 program
+# def fun(d):
+#     for key in d:
+#         print("Key: ", key, " " ,"Value: ", d[key])
+        
+# D = {'a':1, 'b':2, 'c':3, 'd': 4}
+
+# fun(D)
+
+
+
+
+# 21 program
