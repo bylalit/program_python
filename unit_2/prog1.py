9be051a566e66a0b41d3021b8e2425101360279b
Lalit0101 <lalitkp0101@gmail.com>
2024-10-24 22:35:45 +0530

    change program

--- unit_2/prog1.py
+++ unit_2/prog1.py
@@ -67,19 +67,19 @@
 
 
 # 14 program
-# list1 = [i for i in range(0,9)]
-# print(list1)
-# list1.append(12)
-# print(list1)
+list1 = [i for i in range(0,9)]
+print(list1)
+list1.append(12)
+print(list1)
 
-# list1.insert(1, 15)
-# print(list1)
+list1.insert(1, 15)
+print(list1)
 
-# list1.reverse()
-# print(list1)
+list1.reverse()
+print(list1)
 
-# list1.remove(7)
-# print(list1)
+list1.remove(7)
+print(list1)
 
 
 
