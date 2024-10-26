210aec1ae072db66a5e9191c26548fe40bae2a93
Lalit0101 <lalitkp0101@gmail.com>
2024-10-26 22:45:43 +0530

    okk new change

--- unit_2/prog1.py
+++ unit_2/prog1.py
@@ -66,17 +66,17 @@
 
 
 
-# 14 program
-# list1 = [i for i in range(0,9)]
-# print(list1)
-# list1.append(12)
-# print(list1)
+14 program
+list1 = [i for i in range(0,9)]
+print(list1)
+list1.append(12)
+print(list1)
 
-# list1.insert(1, 15)
-# print(list1)
+list1.insert(1, 15)
+print(list1)
 
-# list1.reverse()
-# print(list1)
+list1.reverse()
+print(list1)
 
 # list1.remove(7)
 # print(list1)
