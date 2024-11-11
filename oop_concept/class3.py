c56a65f259ceded774d51e326fe10e2615146b57
Lalit0101 <lalitkp0101@gmail.com>
2024-11-11 21:24:46 +0530

    okk ediat the data

--- oop_concept/class3.py
+++ oop_concept/class3.py
@@ -10,23 +10,23 @@ class Human:
     def work(self):
         print("Human is working")
         
-class Male(Human):
-    def __init__(self, name, heart):
-        super().__init__(heart)
-        self.name = name
+# class Male(Human):
+#     def __init__(self, name, heart):
+#         super().__init__(heart)
+#         self.name = name
         
-    def flirt(self):
-        print("Male is flirting")
+#     def flirt(self):
+#         print("Male is flirting")
         
-    def work(self):
-        super().work()
-        print("I can working coding")
+#     def work(self):
+#         super().work()
+#         print("I can working coding")
         
-    def diaplay(self):
-        print(f"Hi, I am {self.name} and I have {self.num_heart} heart.")
+#     def diaplay(self):
+#         print(f"Hi, I am {self.name} and I have {self.num_heart} heart.")
 
-print()
-male_1 = Male("Lalit", 2)
+# print()
+# male_1 = Male("Lalit", 2)
 # male_1.flirt()
 # male_1.work()
 # print(male_1.num_eyes)
