b4dca33aef457a41371dc2bf3f95f9c7e078f3e8
Lalit0101 <lalitkp0101@gmail.com>
2024-11-11 21:20:00 +0530

    new aediet

--- mini-project/bank_operat_oop.py
+++ mini-project/bank_operat_oop.py
@@ -9,7 +9,6 @@ class Bank:
         self.username = username
         self.pan = pan
         self.address = address
-        self.balance = 0.0
         print(f'Hello {self.username} cong! your account created successfully!')
 
 
