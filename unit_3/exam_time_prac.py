a12b7f396cea4c61ed4080a35a22244e20a994b8
Lalit0101 <lalitkp0101@gmail.com>
2024-11-27 20:06:22 +0530

    new compale

--- unit_3/exam_time_prac.py
+++ unit_3/exam_time_prac.py
@@ -15,16 +15,16 @@
 
 # pro 17
 
-class myClass:
-    def sum(self, a=None, b=None, c=None):
-        if a != None and b != None and c != None:
-            print("Sum of three numbers is ", a + b + c)
-        elif a != None and b != None:
-            print("Sum of two numbers is ", a + b)
-        else:
-            print("Please enter 2 or 3 arguments")
+# class myClass:
+#     def sum(self, a=None, b=None, c=None):
+#         if a != None and b != None and c != None:
+#             print("Sum of three numbers is ", a + b + c)
+#         elif a != None and b != None:
+#             print("Sum of two numbers is ", a + b)
+#         else:
+#             print("Please enter 2 or 3 arguments")
             
-m = myClass()
-m.sum(10, 20)
-m.sum(10, 20, 30)
-m.sum(10)
+# m = myClass()
+# m.sum(10, 20)
+# m.sum(10, 20, 30)
+# m.sum(10)
