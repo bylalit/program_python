a94b72bd3af364cd74ace86bbc83d60c1cd7d69f
Lalit0101 <lalitkp0101@gmail.com>
2024-11-28 22:47:10 +0530

    okk done

--- unit_3/exam_time_prac.py
+++ unit_3/exam_time_prac.py
@@ -83,33 +83,33 @@
 
 # pro 14
 
-# class a:
-#     def method(self):
-#         print("A class method")
-#         super().method()
-# class b:
-#     def method(self):
-#         print("B class method")
-#         super().method()
-# class c:
-#     def method(self):
-#         print("C class method")
-#         super().method()
-# class x(a,b):
-#     def method(self):
-#         print("X class method")
-#         super().method()
-# class y(b,c):
-#     def method(self):
-#         print("Y class method")
-#         super().method()
-#         super().method()
-# class p(x,y,c):
-#     def method(self):
-#         print("P class method")
-#         super().method()
+class a:
+    def method(self):
+        print("A class method")
+        super().method()
+class b:
+    def method(self):
+        print("B class method")
+        super().method()
+class c:
+    def method(self):
+        print("C class method")
+        super().method()
+class x(a,b):
+    def method(self):
+        print("X class method")
+        super().method()
+class y(b,c):
+    def method(self):
+        print("Y class method")
+        super().method()
+        super().method()
+class p(x,y,c):
+    def method(self):
+        print("P class method")
+        super().method()
         
         
-p1 = p()
-print(p.mro())
-p1.method()
+# p1 = p()
+# print(p.mro())
+# p1.method()
