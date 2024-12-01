79bb788e4e762a11943c2d32eb6757f6d6ed70b0
Lalit0101 <lalitkp0101@gmail.com>
2024-12-01 11:34:23 +0530

    add some thing

--- mini-project/hotel_full_pro.py
+++ mini-project/hotel_full_pro.py
@@ -10,3 +10,19 @@ while True:
     main = {}
     desart = {}
     
+    print("""-------------------------------------------------------------
+          1. Started menu add then press 1
+          2. Main menu add then press 2
+          3. Desart menu add then press 3""")
+    
+    choice = int(input("Choice One Option: "))
+    
+    if choice == 1:
+        addDish = int(input("How many dish add in started: "))
+        
+        while addDish < 0:
+            dish = input("Enter the Dish name: ")
+            price = input("Enter the Dish price: ")
+            
+            started[dish] = price
+            
