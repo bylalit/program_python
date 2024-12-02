e9a0af298b2e06ba443a09e90015555f55bf337b
Lalit0101 <lalitkp0101@gmail.com>
2024-12-02 10:05:24 +0530

    done 2 program

---
+++ unit_3/last_exam.py
@@ -0,0 +1,36 @@
+# 1
+# class student:
+#     def __init__(self):
+#         self.name = "Rakesh"
+#         self.age = 20
+#         self.marks = 89
+        
+#     def display(self):
+#         print("Name: ", self.name)
+#         print("Age: ", self.age)
+#         print("Marks: ", self.marks)
+        
+# student = student()
+# student.display()
+
+
+
+# 2 
+
+# class student:
+#     def __init__(self, name, ag=15, m=0):
+#         self.name = name
+#         self.ag = ag
+#         self.m = m
+        
+        
+#     def display(self):
+#         print("Name: ", self.name)
+#         print("Age: ", self.ag)
+#         print("Marks: ", self.m)
+        
+
+# stud = student("Rakesh", 20, 52)
+# stud.display()
+
+
