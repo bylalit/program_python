c5463da82e9a771aa2faae2c8e71c935d1a6457a
Lalit0101 <lalitkp0101@gmail.com>
2024-12-02 10:13:41 +0530

    done half

--- unit_3/last_exam.py
+++ unit_3/last_exam.py
@@ -34,3 +34,36 @@
 # stud.display()
 
 
+
+# class sampal:
+#     var = 10
+#     @classmethod
+    
+#     def new_modify(cls):
+#         cls.var += 1
+        
+# s1 = sampal()
+# s2 = sampal()
+# print("X in s1: ", s1.var)
+# print("X in s2: ", s2.var)
+
+# s1.new_modify()
+# print("X in s1: ", s1.var)
+# print("X in s2: ", s2.var)
+
+
+# class sampal:
+#     def __init__(self):
+#         self.x = 10
+        
+#     def modify(self):
+#         self.x += 1
+        
+# s1 = sampal()
+# s2 = sampal()
+# print("X in s1: ", s1.x)
+# print("X in s2: ", s2.x)
+
+# s1.modify()
+# print("X in s1: ", s1.x)
+# print("X in s2: ", s2.x)
