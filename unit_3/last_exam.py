525d8be419766dc8471447cb994797e54733f418
Lalit0101 <lalitkp0101@gmail.com>
2024-12-02 10:29:04 +0530

    4 program complate

--- unit_3/last_exam.py
+++ unit_3/last_exam.py
@@ -67,3 +67,43 @@
 # s1.modify()
 # print("X in s1: ", s1.x)
 # print("X in s2: ", s2.x)
+
+
+
+
+
+# 4 
+# class student:
+#     def setname(self, name):
+#         self.name =name
+        
+#     def getname(self):
+#         return self.name
+    
+#     def setmarks(self, marks):
+#         self.marks = marks
+        
+#     def getmarks(self):
+#         return self.marks
+    
+# n = int(input("How many students ?: "))
+# i= 0
+
+# while i < n:
+#     s = student()
+#     name = input("Enter Your name: ")
+#     s.setname(name)
+#     marks = int(input("Enter Your marks: "))
+#     s.setmarks(marks)
+#     print("Hii ", s.getname())
+#     print("Your Marks : ", s.getmarks())
+    
+#     i += 1
+#     print("------------------------------------------")
+
+
+
+
+
+
+
