4f912aece9a6b35d2bf39575db44d99887582d30
Lalit0101 <lalitkp0101@gmail.com>
2024-12-11 22:46:45 +0530

    okk

--- unit_3/last_exam.py
+++ unit_3/last_exam.py
@@ -675,16 +675,16 @@
 
 import datetime
 
-year = int(input("Enter your birth year: "))
-month = int(input("Enter your birth month: "))
-day = int(input("Enter your birth date: "))
+# year = int(input("Enter your birth year: "))
+# month = int(input("Enter your birth month: "))
+# day = int(input("Enter your birth date: "))
 
-birthday = datetime.datetime(year, month, day)
-print(birthday)
+# birthday = datetime.datetime(year, month, day)
+# print(birthday)
 
-now = datetime.datetime.now()
-print(birthday)
+# now = datetime.datetime.now()
+# print(birthday)
 
-Diffrent = now - birthday
+# Diffrent = now - birthday
 
-print("Diffrent Is: ", Diffrent.days)
+# print("Diffrent Is: ", Diffrent.days)
