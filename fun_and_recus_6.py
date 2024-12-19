7f83b0d45fbd0d2ae523fa267a3cc6537d0b6e1e
Lalit0101 <lalitkp0101@gmail.com>
2024-12-19 22:40:36 +0530

    code error

--- fun_and_recus_6.py
+++ fun_and_recus_6.py
@@ -12,20 +12,20 @@ def fac(n):
 # print(fac(5))  # Output: 120
 
 
-def evenodd(a):
-    if a % 2 == 0:
-        print("even")
-    else:
-        print("odd")
+# def evenodd(a):
+#     if a % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
         
         
-# evenodd(8)
+# # evenodd(8)
 
 
-def show(n):
-    if n == 0:
-        return
-    print(n)
-    show(n-1)
+# def show(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     show(n-1)
     
-show(5)
+# show(5)
