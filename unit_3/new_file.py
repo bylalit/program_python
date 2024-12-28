38cdceece89c750720f3bce74818a5d99e603eae
Lalit0101 <lalitkp0101@gmail.com>
2024-12-28 22:24:02 +0530

    done the

--- unit_3/new_file.py
+++ unit_3/new_file.py
@@ -23,20 +23,68 @@
     
     
     
-print(0)
-print(1)
-count = 2
+# print(0)
+# print(1)
+# count = 2
 
-def fibonacci(prev1, prev2):
+# def fibonacci(prev1, prev2):
+#     global count
+#     if count <= 19:
+#         newFibo = prev1 + prev2
+#         print(newFibo, end = " ")
+#         prev2 = prev1
+#         prev1 = newFibo
+#         count += 1
+#         fibonacci(prev1, prev2)
+#     else:
+#         return
+
+# fibonacci(1,0)
+
+
+
+# print(0,1, end=" ")
+count = 2
+sum = 0
+
+def fibo(prev1, prev2):
     global count
-    if count <= 19:
-        newFibo = prev1 + prev2
-        print(newFibo)
-        prev2 = prev1
-        prev1 = newFibo
+    global sum
+    if count < 20:
+        new = prev1 + prev2
+        sum += new
+        # print(new, end = " ")
+        prev1 = prev2
+        prev2 = new
         count += 1
-        fibonacci(prev1, prev2)
+        fibo(prev1, prev2)
     else:
         return
+    # print(sum)
+    
+# fibo(0,1)
+# print(sum)
 
-fibonacci(1,0)
+
+
+
+my_array = [64, 34, 25, 12, 22, 11, 90, 5]
+
+n = len(my_array)
+for i in range(n-1):
+    for j in range(n-i-1):
+        if my_array[j] > my_array[j+1]:
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+
+print("Sorted array:", my_array)
+
+# a = []
+# for i in my_array:
+#     a = a + [i]
+#     reversed(i)
+#     # print(i, end=" ")
+#     # print(my_array[:-1])
+    
+# print(a)
+
+print(reversed(my_array))
