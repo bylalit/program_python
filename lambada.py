47b16802a7a2da3437aae7c7a22973e254b76f60
Lalit0101 <lalitkp0101@gmail.com>
2024-12-29 10:02:29 +0530

    okk done the code

---
+++ lambada.py
@@ -0,0 +1,19 @@
+# f = lambda x,y : x + y
+# print(f(5,8))
+
+
+# f = lambda a : a*a*a
+# print(f(5))
+
+
+# f = lambda a, b : a if a > b else b
+# print(f(52,8))
+
+
+
+# f = lambda a, b : a if a % 2 == 0 else b
+# print(f(5,54))
+
+
+
+
