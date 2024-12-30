4d77eea6743cb5532c53c3718abef560d5c030fc
Lalit0101 <lalitkp0101@gmail.com>
2024-12-30 14:14:41 +0530

    okk

--- fun_and_recus_6.py
+++ fun_and_recus_6.py
@@ -20,10 +20,54 @@ def evenodd(a):
         
 # evenodd(8)
 
-def show(n):
-    if n == 0:
-        return
-    print(n)
-    show(n-1)
+# def show(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     show(n-1)
     
-show(5)
+# show(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+menu = {
+    'tave': 20,
+    'paneer': 30,
+    'pizaa': 40,
+    'burger': 80
+}
+
+menu['dosha'] = 90
+
+print(menu)
+
+# if "dosha" not in menu:
+#     print("Byy")
+# else:
+#     print('Hello')
+#     menu['mendu'] = 20
+# print(menu)
+
+
+# for i,a in menu.items():
+    # print(i,a)
+    
+# for i in menu:
+#     print(menu[i])
+
+# print(menu.keys())
+# print(menu.values())
+# print(menu.items())
+# print(menu.get('tave'))
+# print(menu['pizaa'])
+
