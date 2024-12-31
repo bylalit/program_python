0cd81171f7216b6c50ccb561fff74ea8e9c2abf4
Lalit0101 <lalitkp0101@gmail.com>
2024-12-31 10:14:02 +0530

    okk done

--- array.py
+++ array.py
@@ -1,29 +1,39 @@
-# import array
-from array import *
+# # import array
+# from array import *
 
 
-vals = array('i', [5,9,-7,5,2,8])
-# vals.reverse()
+# vals = array('i', [5,9,-7,5,2,8])
+# # vals.reverse()
 
-# for i in range(len(vals)):
-#     print(vals[i])
+# # for i in range(len(vals)):
+# #     print(vals[i])
 
 
-new_array = array(vals.typecode, (i*i for i in vals))
+# new_array = array(vals.typecode, (i*i for i in vals))
 
-new_array.append(89)
-# for e in new_array:
-#     print(e)
+# new_array.append(89)
+# # for e in new_array:
+# #     print(e)
     
-a = sorted(new_array)
+# a = sorted(new_array)
 
-print(a)
-a.reverse()
-print(a)
+# print(a)
+# a.reverse()
+# print(a)
 
-sum = 0
-for i in a:
-    # print(i)
-    sum += i
+# sum = 0
+# for i in a:
+#     # print(i)
+#     sum += i
     
-print(sum)
+# print(sum)
+
+
+
+
+
+
+
+
+
+
