811b435cde39fe3e95b0be529f65432777f986ce
Lalit0101 <lalitkp0101@gmail.com>
2025-01-02 21:25:17 +0530

    new feature

--- apis.py
+++ apis.py
@@ -1,27 +1,27 @@
 # import requests
 
-# def feacth_api_user():
-#     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
-#     response = requests.get(url)
-#     data = response.json()
+def feacth_api_user():
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
+    response = requests.get(url)
+    data = response.json()
     
-#     if data["success"] and "data" in data:
-#         user_data = data["data"]
-#         username = user_data["login"]["username"]
-#         country = user_data["location"]["country"]
-#         return username, country
-#     else:
-#         raise Exception("failed to fecth user data")
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        country = user_data["location"]["country"]
+        return username, country
+    else:
+        raise Exception("failed to fecth user data")
     
-# def main():
-#     try:
-#         username, country = feacth_api_user()
-#         print(f"Username: {username} \nCountry: {country}")
-#     except Exception as e:
-#         print(f"Error: {str(e)}")
+def main():
+    try:
+        username, country = feacth_api_user()
+        print(f"Username: {username} \nCountry: {country}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
     
     
 
