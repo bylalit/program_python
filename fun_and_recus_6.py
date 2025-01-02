87a4df0020dae4e58edd8c5a3fcf30aab26a4b0c
Lalit0101 <lalitkp0101@gmail.com>
2025-01-02 21:24:55 +0530

    okk

--- fun_and_recus_6.py
+++ fun_and_recus_6.py
@@ -3,20 +3,20 @@
     
 # hel()
 
-def fac(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fac(n-1)
+# def fac(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * fac(n-1)
     
 # print(fac(5))  # Output: 120
 
 
-# def evenodd(a):
-#     if a % 2 == 0:
-#         print("even")
-#     else:
-#         print("odd")
+def evenodd(a):
+    if a % 2 == 0:
+        print("even")
+    else:
+        print("odd")
         
 # evenodd(8)
 
